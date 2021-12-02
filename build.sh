aa0b5d75cfeba638488424feac766ef2b8c0abce
JzuvGTI <70012176+JzuvGTI@users.noreply.github.com>
2021-12-02 08:55:15 -0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,13 @@
+# If you already install g++ or build-essential, use this
+clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o gtbot -w #Build source code into executable
+echo ""
+echo "================================="
+echo "⚠This Script Remaked By JzuvGTI⚠"
+echo ""
+echo "Tool CID Successfully Builded,"
+echo ""
+echo "Kamu bisa ketik ./gtbot sekarang!!!"
+echo " "
+echo "©Credit ZaqXkun GT For The Script"
+echo "|Remaked By JzuvGTI|"
+echo "================================="
